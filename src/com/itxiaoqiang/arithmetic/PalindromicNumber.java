7bd0a90591f54a512dd7a75b377de47e279d8926
lrq154439 <2990599771@qq.com>
2019-05-04 18:45:00 +0800

    修改

---
+++ src/com/itxiaoqiang/arithmetic/PalindromicNumber.java
@@ -0,0 +1,13 @@
+package com.itxiaoqiang.arithmetic;
+
+/**
+ * 判断一个整数是否是平方回文数
+ */
+public class PalindromicNumber {
+
+    public static boolean isPalindromic(int n) {
+
+        return false;
+
+    }
+}
