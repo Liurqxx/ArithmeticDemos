b8e289978f0f29278d1f3abc424fd23b42cb67f2
lrq154439 <2990599771@qq.com>
2019-05-05 17:45:42 +0800

    添加

--- src/com/itxiaoqiang/arithmetic/DaffodilNumber.java
+++ src/com/itxiaoqiang/arithmetic/DaffodilNumber.java
@@ -7,6 +7,26 @@ public class DaffodilNumber {
 
     public static boolean isDaffodil(int n) {
 
+        long i, start, end, temp, num, sum;
+        int j;
+
+        start = (long) Math.pow(10, n - 1);
+        end = (long) Math.pow(10, n) - 1;
+
+        for (long k = start; k <= end; k++) {
+            temp = 0;
+            num = k;
+            sum = 0;
+            for (int l = 0; l < n; l++) {
+                temp = num % 10;
+                sum = sum + (long) Math.pow(temp, n);
+                num = (num - temp) / 10;
+            }
+            if (sum == k) {
+                return true;
+            }
+        }
+
         return false;
 
     }
