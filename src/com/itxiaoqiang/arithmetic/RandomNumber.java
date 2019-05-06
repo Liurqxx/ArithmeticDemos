9f008bdc9dc610a98a156a698df9f9605b10b5ed
lrq154439 <2990599771@qq.com>
2019-05-06 16:50:41 +0800

    修改

---
+++ src/com/itxiaoqiang/arithmetic/RandomNumber.java
@@ -0,0 +1,16 @@
+package com.itxiaoqiang.arithmetic;
+
+/**
+ * [0-1]之间均匀分布的随机数算法
+ */
+public class RandomNumber {
+
+    public static double getRandomNum(double[] r) {
+
+
+        return 1.00;
+
+    }
+
+
+}
