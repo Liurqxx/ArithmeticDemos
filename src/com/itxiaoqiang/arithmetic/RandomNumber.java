a5bfda31f9e6eb815ad1af5ac021a2b2298144dc
lrq154439 <2990599771@qq.com>
2019-05-06 16:55:35 +0800

    修改

--- src/com/itxiaoqiang/arithmetic/RandomNumber.java
+++ src/com/itxiaoqiang/arithmetic/RandomNumber.java
@@ -7,8 +7,17 @@ public class RandomNumber {
 
     public static double getRandomNum(double[] r) {
 
+        double base, u, v, p, temp1, temp2, temp3;
+        base = 256.0;//基数
+        u = 17.0;
+        v = 139.0;
 
-        return 1.00;
+        temp1 = u * (r[0]) + v;//计算总值
+        temp2 = (int) (temp1 / base);//计算商
+        temp3 = temp1 - temp2 * base;//计算余数
+        r[0] = temp3;//更新随机种子
+        p = r[0] / base;//随机数
+        return p;
 
     }
 
